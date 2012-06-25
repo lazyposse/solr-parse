59f2931a270cc856f0eec6feed745bdd983a8062
Denis Labaye <denis.labaye@gmail.com>
2012-06-25 17:53:54 +0200

    dup-par

--- src/solr_parse/eval.clj
+++ src/solr_parse/eval.clj
@@ -168,12 +168,44 @@
 
 (defn- rm-nil "Given a nested datastructure, returns the same but without nil"
   [s] (if (sequential? s)
-        (map f (remove nil? s))
+        (map rm-nil (remove nil? s))
         s))
 
 (fact "rm-nil"
   (rm-nil '(nil)) => ()
-  (rm-nil '((nil))) => '(()))
+  (rm-nil '((nil))) => '(())
+  (rm-nil 'a) => 'a
+  (rm-nil '(a nil a nil)) => '(a a))
+
+(defn- dup-par?
+  [s] (and (sequential? s)
+           (sequential? (first s))
+           (not (second s))))
+
+(fact "dup-par?"
+  (dup-par? :a) => false
+  (dup-par? '(:a)) => false
+  (dup-par? '((:a))) => true)
+
+(defn- rm-dup-par-1 "given a form 'peel' its parens til it only one level"
+  [s] (if (dup-par? s)
+        (rm-dup-par-1 (first s))
+        s))
+
+(fact "rm-dup-par-1"
+  (rm-dup-par-1 'a)   => 'a
+  (rm-dup-par-1 '(a))   => '(a)
+  (rm-dup-par-1 '((a)))   => '(a)
+  (rm-dup-par-1 '(((a)))) => '(a))
+
+(defn- rm-dup-par "Remove duplicate parens from the given form (recursively)"
+  [s] (if (sequential? s)
+        (map rm-dup-par
+             (rm-dup-par-1 s))
+        s))
+
+(fact "rm-dup-par"
+  (rm-dup-par '((a (b) ((c))))) => '(a (b) (c)))
 
 (defmethod to-query :expr-par
   [{s :content}] (map (fn [x] (if (sequential? x)
